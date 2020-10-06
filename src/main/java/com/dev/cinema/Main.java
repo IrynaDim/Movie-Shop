3272e6a7fbbbf68b2f1252334ffa5c3cb851fd6d
Your Name <i.dymytreyeva@gmail.com>
2020-10-06 15:10:28 +0300

    solution added

--- src/main/java/com/dev/cinema/Main.java
+++ src/main/java/com/dev/cinema/Main.java
@@ -1,8 +1,15 @@
 package com.dev.cinema;
 
 import com.dev.cinema.lib.Injector;
+import com.dev.cinema.model.CinemaHall;
 import com.dev.cinema.model.Movie;
+import com.dev.cinema.model.MovieSession;
+import com.dev.cinema.service.CinemaHallService;
 import com.dev.cinema.service.MovieService;
+import com.dev.cinema.service.MovieSessionService;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Main {
     private static Injector injector = Injector.getInstance("com.dev.cinema");
@@ -10,8 +17,27 @@ public class Main {
     public static void main(String[] args) {
         Movie movie = new Movie();
         movie.setTitle("Fast and Furious");
+        movie.setDescription("movie about street racing");
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
         movieService.add(movie);
         movieService.getAll().forEach(System.out::println);
+
+        CinemaHallService cinemaHallService = (CinemaHallService) injector
+                .getInstance(CinemaHallService.class);
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(100);
+        cinemaHall.setDescription("the biggest cinema hall");
+        cinemaHallService.add(cinemaHall);
+        System.out.println(cinemaHallService.getAll());
+
+        MovieSession movieSession = new MovieSession();
+        movieSession.setCinemaHall(cinemaHall);
+        movieSession.setMovie(movie);
+        movieSession.setShowTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(20, 00)));
+        MovieSessionService movieSessionService = (MovieSessionService) injector
+                .getInstance(MovieSessionService.class);
+        movieSessionService.add(movieSession);
+        System.out.println(movieSessionService
+                .findAvailableSessions(movie.getId(), LocalDate.now()));
     }
 }
